fba3b08a8e11cc6ad523df7676822ccc7f7c1722
MikE Monroe <redfive.m@gmail.com>
2013-08-03 16:47:23 -0700

    Adding a dummy object that will spawn Reflector prefabs, that way I can modify the Reflector prefab and not worry about making sure the instances in a scene are up to date.

--- Assets/Scripts/ReflectorInstigatorHandler.cs
+++ Assets/Scripts/ReflectorInstigatorHandler.cs
@@ -5,10 +5,12 @@ public class ReflectorInstigatorHandler : MonoBehaviour {
 	
 	public float randomScaleAmount;
 	public Vector2 randomForceAmount;
+	public Vector3 randomInertiaTensorAmount;
 	public float randomSpinAmount;
 	
 	private float randomScale;
 	private Vector2 randomForce;
+	private Vector3 randomIntertiaTensor;
 	private float randomSpin;
 	
 	private Transform cachedTransform;
@@ -17,9 +19,14 @@ public class ReflectorInstigatorHandler : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
-		randomScale = Random.Range(0F,randomScaleAmount);
+		randomScale = Random.Range(.25F,randomScaleAmount);
 		randomForce.x = Random.Range(0F, randomForceAmount.x);
 		randomForce.y = Random.Range(0F, randomForceAmount.y);
+		
+		randomIntertiaTensor.x = Random.Range(0F, randomInertiaTensorAmount.x);
+		randomIntertiaTensor.y = Random.Range(0F, randomInertiaTensorAmount.y);
+		randomIntertiaTensor.z = Random.Range(0F, randomInertiaTensorAmount.z);
+		
 		randomSpin = Random.Range(0F, randomSpinAmount);
 		
 		cachedTransform = gameObject.transform;
@@ -27,6 +34,8 @@ public class ReflectorInstigatorHandler : MonoBehaviour {
 		
 		cachedTransform.localScale = new Vector3 (randomScale, randomScale, randomScale);
 		cachedRigidbody.AddForce(randomForce.x, randomForce.y,0);
+		cachedRigidbody.inertiaTensor = new Vector3(
+			randomIntertiaTensor.x, randomIntertiaTensor.y, randomIntertiaTensor.z);
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/ReflectorSpawner.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReflectorSpawner : MonoBehaviour {
+	
+	public GameObject prefabToSpawn;
+	void Awake () 
+	{
+		GameObject newObj = Instantiate(prefabToSpawn, transform.position, transform.rotation) as GameObject;
+		Destroy(gameObject);
+	}
+}
