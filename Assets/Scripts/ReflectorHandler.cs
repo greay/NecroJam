f9a49b333f9c83b31cc5b704fcd6c19cf1b85625
MikE Monroe <redfive.m@gmail.com>
2013-08-03 13:21:04 -0700

    Mike adding his initial assets to NecroJam woo

---
+++ Assets/Scripts/ReflectorHandler.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReflectorHandler : MonoBehaviour {
+	
+	
+	public float reflectLength = .5F;
+	public Animation emitAnimationRef;
+	
+	private bool hasReflected = false;
+	
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+	
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "reflector" && !hasReflected)
+		{
+			EmitReflection();
+		}
+	}
+	
+	void EmitReflection()
+	{
+		emitAnimationRef.Play();
+	}
+	
+}
