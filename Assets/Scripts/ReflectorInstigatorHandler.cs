75205c77e71065273cb3517a8a226f39855bc03d
MikE Monroe <redfive.m@gmail.com>
2013-08-03 17:19:11 -0700

    Adding a "GO" button to trigger the Instigator instead of having it automatically go. Also removed a rigidbody from the emit child of Instigator since it was causing funky physics.

--- Assets/Scripts/ReflectorInstigatorHandler.cs
+++ Assets/Scripts/ReflectorInstigatorHandler.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class ReflectorInstigatorHandler : MonoBehaviour {
 	
+	public Animation emitAnimationRef;
+	
 	public float randomScaleAmount;
 	public Vector2 randomForceAmount;
 	public Vector3 randomInertiaTensorAmount;
@@ -15,11 +17,20 @@ public class ReflectorInstigatorHandler : MonoBehaviour {
 	
 	private Transform cachedTransform;
 	private Rigidbody cachedRigidbody;
+	private bool spinActive=false;
 	
 	// Use this for initialization
 	void Start () 
 	{
+		RandomizeValues();
+		cachedTransform = gameObject.transform;
+		cachedRigidbody = gameObject.rigidbody;
+	}
+	
+	void RandomizeValues()
+	{
 		randomScale = Random.Range(.25F,randomScaleAmount);
+		
 		randomForce.x = Random.Range(0F, randomForceAmount.x);
 		randomForce.y = Random.Range(0F, randomForceAmount.y);
 		
@@ -28,22 +39,29 @@ public class ReflectorInstigatorHandler : MonoBehaviour {
 		randomIntertiaTensor.z = Random.Range(0F, randomInertiaTensorAmount.z);
 		
 		randomSpin = Random.Range(0F, randomSpinAmount);
-		
-		cachedTransform = gameObject.transform;
-		cachedRigidbody = gameObject.rigidbody;
-		
+	}
+	
+	void Update () 
+	{
+		if (randomSpinAmount != 0 && randomSpin != 0 && spinActive)
+		{
+			cachedTransform.Rotate(0,randomSpin,0);
+		}
+	}
+	
+	public void ActivateInstigator()
+	{
+		spinActive=true;
 		cachedTransform.localScale = new Vector3 (randomScale, randomScale, randomScale);
 		cachedRigidbody.AddForce(randomForce.x, randomForce.y,0);
 		cachedRigidbody.inertiaTensor = new Vector3(
 			randomIntertiaTensor.x, randomIntertiaTensor.y, randomIntertiaTensor.z);
+		emitAnimationRef.Play();
 	}
 	
-	// Update is called once per frame
-	void Update () 
-	{
-		if (randomSpinAmount != 0 && randomSpin != 0)
-		{
-			cachedTransform.Rotate(0,randomSpin,0);
-		}
+	void OnGUI()
+	{	
+        if (GUI.Button(new Rect(10, 70, 50, 30), "GO"))     
+			ActivateInstigator();
 	}
 }
